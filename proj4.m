be9d7bee2af9380a06e6bb472928a2dc09e8a632
dolphinscathy <liuxiyuan95@gmail.com>
2018-04-24 17:25:42 +0800

    for reference only

--- proj4.m
+++ proj4.m
@@ -1,3 +1,7 @@
-path = 'data02\';
-[resampled_img, light_dir] = uniformResample(path);
+path = 'data02/';
+lambda = 0.5;
+sigma = 0.6;
+[resampled_img,light_dir] = uniformResample(path);
 ini_normal = getInitialNormal(resampled_img, light_dir);
+ref_normal = refineNormal(ini_normal,lambda,sigma);
+recSurf=surfReconstruct(ref_normal,2);
