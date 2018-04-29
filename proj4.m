e468cf0bed99bdd8673d2d56c2ae407426044f04
dolphinscathy <liuxiyuan95@gmail.com>
2018-04-29 20:16:53 +0800

    add normal image

--- proj4.m
+++ proj4.m
@@ -1,20 +1,24 @@
-path = 'data08/';
-[resampled_images,light_direction] = uniformResampling(path);
+path = 'data09/';
+[resampled_images,light_direction] = uniformResampling(path,507);
 initial_normal = getInitialNormal(resampled_images,light_direction);
 %surf = shapeFromNormal(initial_normal,2,I);
+imshow((-1/sqrt(3)*initial_normal(:,:,1) + 1/sqrt(3)*initial_normal(:,:,2) + 1/sqrt(3)*initial_normal(:,:,3))/1.1);
 refined_normal = refineNormal(initial_normal,0.5,0.65);
 surf = shapeFromNormal(refined_normal,6,I);
+imshow((-1/sqrt(3)*refined_normal(:,:,1) + 1/sqrt(3)*refined_normal(:,:,2) + 1/sqrt(3)*refined_normal(:,:,3))/1.1);
 
 %some parameters
 %           lambda   sigma   scale   pointer
 % data02      0.5     0.6     2        641
-% data04      0.5     0.6     6        
+% data04      0.5     0.6     6        769
 % data05      0.5     0.6     4        
 % data06      0.5     0.8     4        
 % data07      0.5     0.4     8        
 % data08      0.5     0.4     6        950
-% data09      0.4     0.6     4        
-% data09      0.5     0.6     4    
+% data09      0.4     0.6     4        707
+% data10      0.5     0.6     4        2034
+
+
 image_files = dir(fullfile(path,'*.bmp'));
 I = imread(fullfile(path,image_files(950).name));
 I = flip(I,1);
